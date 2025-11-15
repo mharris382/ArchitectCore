fde12ce068787a25e9a64467e77265a74cc97615
Max H <mharris382@gmail.com>
2025-11-14 20:44:45 -0500

    beginning recipe refactor and C++ implementation

--- Source/ArchitectCore/ArchitectCore.Build.cs
+++ Source/ArchitectCore/ArchitectCore.Build.cs
@@ -26,7 +26,8 @@ public class ArchitectCore : ModuleRules
 			new string[]
 			{
 				"Core",
-                "PCG"
+                "PCG",
+                "PCGExtendedToolkit"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
